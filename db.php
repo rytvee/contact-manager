d427745015da4cb8f7416915b4e8319e9e2c1926
Nwachukwu Victory Bethel <Victorybethel95@gmail.com>
2025-08-14 09:01:55 +0100

    Update db.php

--- db.php
+++ db.php
@@ -3,14 +3,12 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'contact_manager';
-$port = 3306;
 
 // Create connection
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-
