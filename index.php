79c2fe3108576c95f4f94ae5de9c4e730429aa99
Nwachukwu Victory Bethel <Victorybethel95@gmail.com>
2026-02-27 16:33:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ include 'db.php';
 <!DOCTYPE html>  
 <html lang="en">  
 <head>  
-    <meta charset="UTF-8">  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Manager</title>  
     <link rel="icon" href="images/favicon.ico">  
     <link rel="stylesheet" href="css/style.css">
